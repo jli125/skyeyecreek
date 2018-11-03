db6aae11c62e58c429333576362dc52b455623d8
Jamie Li <jamie.jli125@gmail.com>
2018-11-03 13:33:49 -0700

    api v1 GET user

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 # define the app for the module
 app = Flask(__name__)
 
-@app.route("/api/v1/info")
+@app.route('/api/v1/info')
 def home_index():
     conn = sqlite3.connect('mydb.db')
     print("Opened database successfully");
@@ -20,6 +20,26 @@ def home_index():
         api_list.append(a_dict)
     conn.close()
     return jsonify({'api_version':api_list}),200
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return list_users();
+
+def list_users():
+    conn=sqlite3.connect('mydb.db')
+    print("Opened database successfully");
+    api_list=[]
+    cursor = conn.execute("SELECT username, full_name, email, password, id from users")
+    for row in cursor:
+        a_dict = {}
+        a_dict['username'] = row[0]
+        a_dict['name'] = row[1]
+        a_dict['email'] = row[2]
+        a_dict['password'] = row[3]
+        a_dict['id'] = row[4]
+        api_list.append(a_dict)
+    conn.close()
+    return jsonify({'user_list':api_list})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
