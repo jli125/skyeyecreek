a4ee47d0257f6c612bc98462dbef9dea190bf626
Jamie Li <jamie.jli125@gmail.com>
2018-11-04 09:40:37 -0800

    api v1 POST users

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request, abort
 import json
 import sqlite3
 
@@ -63,9 +63,45 @@ def list_user(user_id):
     conn.close()
     return jsonify(api_list)
 
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = {
+        'username':request.json['username'],
+        'email':request.json['email'],
+        'name':request.json.get('name',""),
+        'password':request.json['password']
+    }
+    return jsonify({'status':add_user(user)}),201
+
+def add_user(new_user):
+    conn = sqlite3.connect('mydb.db')
+    print("Opened database successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? or email=?", (new_user['username'], new_user['email']))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        abort(409)
+    else:
+        cursor.execute("insert into users (username, email, password, full_name) values(?,?,?,?)", (new_user['username'], new_user['email'], new_user['password'], new_user['name']))
+        conn.commit()
+        return "Success"
+    conn.close()
+    return jsonify(a_dict)
+
+@app.errorhandler(400)
+def invalid_request(error):
+    return make_response(jsonify({'error':'Bad Request'}), 400)
+
 @app.errorhandler(404)
 def resource_not_found(error):
     return make_response(jsonify({'error':'Resource not found!'}), 404)
+
+@app.errorhandler(409)
+def user_found(error):
+    return make_response(jsonify({'error':'COnflict! Record exist!'}), 404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
