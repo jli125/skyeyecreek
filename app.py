a939e8b007c31ce2aab62669f78f9282cf6e4e57
Jamie Li <jamie.jli125@gmail.com>
2018-11-10 11:51:59 -0800

    api v1 DELETE users

--- app.py
+++ app.py
@@ -91,6 +91,27 @@ def add_user(new_user):
     conn.close()
     return jsonify(a_dict)
 
+@app.route('/api/v1/users',methods=['DELETE'])
+def delete_user():
+    if not request.json or not 'username' in request.json:
+        abort(400)
+    user=request.json['username']
+    return jsonify({'status':del_user(user)}), 200
+
+def del_user(del_user):
+    conn = sqlite3.connect('mydb.db')
+    print("Opened database successfully");
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from users where username=?", (del_user,))
+    data = cursor.fetchall()
+    print("Data", data)
+    if len(data) == 0:
+        about(404)
+    else:
+        cursor.execute("delete from users where username==?", (del_user,))
+        conn.commit()
+        return "Success"
+
 @app.errorhandler(400)
 def invalid_request(error):
     return make_response(jsonify({'error':'Bad Request'}), 400)
